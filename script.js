5af458c86259ad1f741429baaffd7299e212c033
Dmitriy <67125062+f19911@users.noreply.github.com>
2020-06-28 16:51:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let age = prompt('Please, enter your age');
+console.log(age);
+let experience = prompt('How old is the experience?');
+let degree = prompt('What is your education?')
+
+age >= 30 && experience >= 3 && degree === 'higher education' || degree === 'master' ?
+    alert('You are accepted') :
+    alert('You do not suit us');
+
+
+
+
+
+
+
